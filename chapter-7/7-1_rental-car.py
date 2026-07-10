623a8da4c22603f9751313b18f32d2bd0552ecdb
Tennille Clayton <tennilleclayton@gmail.com>
2026-07-09 20:04:24 -0400

    Initial Commit

---
+++ chapter-7/7-1_rental-car.py
@@ -0,0 +1,6 @@
+# 7-1: Rental Car
+# Write a program that asks the user what kind of rental car they would like. Print a message about that car, such as "Let me see if I can find you a Subaru".
+
+question = "What kind of rental car you want? "
+prompt = input(question)
+print(f"Let me see if I can find you a {prompt}")
